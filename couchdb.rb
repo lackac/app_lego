d9512a102c303cb9029103c2f47c9c10428536ce
Laszlo Bacsi <lackac@lackac.hu>
2008-12-31 15:51:11 +0100

    Fixed order of operations in couchdb module (braid can only operate with no local changes present)

--- couchdb.rb
+++ couchdb.rb
@@ -1,7 +1,7 @@
-gem 'jchris-couchrest', :lib => 'couchrest', :source => 'http://gems.github.com'
-
 plugin 'basic_model',
   :git => 'git://github.com/topfunky/basic_model.git'
+
+gem 'jchris-couchrest', :lib => 'couchrest', :source => 'http://gems.github.com'
 
 git :add => "."
 git :commit => "-a -m 'Added CouchDB support (through CouchRest and BasicModel)'"
