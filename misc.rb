5c8be4ca86836733f7e8548b45afbd565ebd8dee
Laszlo Bacsi <lackac@lackac.hu>
2008-12-31 14:40:56 +0100

    Fixed indentation in generated files

--- misc.rb
+++ misc.rb
@@ -36,8 +36,8 @@ YAML
 if ENV['LOCALES']
   ENV['LOCALES'].split(",").map {|l| l.gsub(/\.(yml|rb)$/, '')}.uniq.each do |locale|
     file "config/locales/#{locale}.app.yml", <<-YAML
-    #{locale}:
-      app_name: "APP_NAME"
+#{locale}:
+  app_name: "APP_NAME"
     YAML
   end
 end
@@ -45,57 +45,57 @@ end
 if File.exists?('vendor/plugins/haml')
 
   file 'app/views/layouts/_flashes.html.haml', <<-HAML
-  #flash
-    - flash.each do |key, value|
-      %div{:id => "flash_\#{key}"}= value
-    - end
+#flash
+  - flash.each do |key, value|
+    %div{:id => "flash_\#{key}"}= value
+  - end
   HAML
 
   file 'app/views/layouts/application.html.haml', <<-HAML
-  !!! XML
-  !!! Strict
-  %html{'xmlns' => http://www.w3.org/1999/xhtml, 'xml:lang' => 'en', 'lang' => 'en'}
-    %head
-      %meta{'http-equiv' => 'Content-Type', :content => 'text/html; charset=utf-8'}
-      %title= "\#{page_title + ' - ' unless page_title.blank?}\#{t(:app_name)}"
-      = stylesheet_link_tag 'screen.css', :media => 'screen, projection'
-      = stylesheet_link_tag 'print.css', :media => 'print'
-      /[if IE]
-        = stylesheet_link_tag 'ie.css', :media => 'screen, projection'
-    %body{:class => body_class}
-      = render :partial => 'layouts/flashes'
-      = yield
+!!! XML
+!!! Strict
+%html{'xmlns' => http://www.w3.org/1999/xhtml, 'xml:lang' => 'en', 'lang' => 'en'}
+  %head
+    %meta{'http-equiv' => 'Content-Type', :content => 'text/html; charset=utf-8'}
+    %title= "\#{page_title + ' - ' unless page_title.blank?}\#{t(:app_name)}"
+    = stylesheet_link_tag 'screen.css', :media => 'screen, projection'
+    = stylesheet_link_tag 'print.css', :media => 'print'
+    /[if IE]
+      = stylesheet_link_tag 'ie.css', :media => 'screen, projection'
+  %body{:class => body_class}
+    = render :partial => 'layouts/flashes'
+    = yield
   HAML
 
 else
 
   file 'app/views/layouts/_flashes.html.erb', <<-ERB
-  <div id="flash">
-    <% flash.each do |key, value| -%>
-      <div id="flash_<%= key %>"><%=h value %></div>
-    <% end -%>
-  </div>
+<div id="flash">
+  <% flash.each do |key, value| -%>
+    <div id="flash_<%= key %>"><%=h value %></div>
+  <% end -%>
+</div>
   ERB
 
   file 'app/views/layouts/application.html.erb', <<-ERB
-  <?xml version='1.0' encoding='utf-8' ?>
-  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-    <head>
-      <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-      <title><%= page_title + ' - ' unless page_title.blank? %><%=t :app_name %></title>
-      <%= stylesheet_link_tag 'screen.css', :media => 'screen, projection' %>
-      <%= stylesheet_link_tag 'print.css', :media => 'print' %>
-      <!--[if IE]>
-      <%= stylesheet_link_tag 'ie.css', :media => 'screen, projection' %>
-      <![endif]-->
-    </head>
-    <body class="<%= body_class %>">
-      <%= render :partial => 'layouts/flashes' -%>
-      <%= yield %>
-    </body>
-  </html>
+<?xml version='1.0' encoding='utf-8' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <title><%= page_title + ' - ' unless page_title.blank? %><%=t :app_name %></title>
+    <%= stylesheet_link_tag 'screen.css', :media => 'screen, projection' %>
+    <%= stylesheet_link_tag 'print.css', :media => 'print' %>
+    <!--[if IE]>
+    <%= stylesheet_link_tag 'ie.css', :media => 'screen, projection' %>
+    <![endif]-->
+  </head>
+  <body class="<%= body_class %>">
+    <%= render :partial => 'layouts/flashes' -%>
+    <%= yield %>
+  </body>
+</html>
   ERB
 
 end
