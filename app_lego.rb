f9259b83bac1173eece849227d00c8d467f2a19f
Laszlo Bacsi <lackac@lackac.hu>
2009-02-06 11:14:37 +0100

    Moved basic setup to its on module

--- app_lego.rb
+++ app_lego.rb
@@ -29,6 +29,7 @@ if use_lego?("braid", "Use braid for vendor management?")
 end
 
 modules = [
+  ["basic",   "Do basic setup? (only exclude this if you already have a Rails app skeleton with Rails 2.3+ frozen, or as a gem)"],
   ["rspec",   "Use RSpec instead of test/unit?"],
   ["haml",    "Use haml for views and sass for css?"],
   ["jquery",  "Use jQuery instead of Prototype + Script.aculo.us?"],
@@ -36,56 +37,6 @@ modules = [
   ["locale",  "Add specific localizations?"],
   ["misc",    "Add miscellaneous stuff (helpers, basic layout, flashes, initializers)?"],
 ]
-
-# remove tmp dirs
-run "rmdir tmp/{pids,sessions,sockets,cache}"
-
-# remove unnecessary stuff
-run "rm README log/*.log public/index.html public/images/rails.png"
-
-# keep empty dirs
-run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;")
-
-# init git repo
-git :init
-
-# basic .gitignore file
-file '.gitignore', 
-%q{log/*.log
-log/*.pid
-db/*.db
-db/*.sqlite3
-db/schema.rb
-tmp/**/*
-.DS_Store
-doc/api
-doc/app
-config/database.yml
-autotest_result.html
-coverage
-public/javascripts/*_[0-9]*.js
-public/stylesheets/*_[0-9]*.css
-public/attachments
-}
-
-# copy sample database config
-run "cp config/database.yml config/database.yml.sample"
-
-# commit changes
-git :add => "."
-git :commit => "-a -m 'Setting up a new rails app. Copy config/database.yml.sample to config/database.yml and customize.'"
-
-# freeze edge rails
-if respond_to?(:braid)
-  braid "git://github.com/rails/rails.git", "vendor/rails"
-else
-  # Guess full Rails path
-  rails_path = Pathname.new($LOAD_PATH.find {|p| p =~ /railties/}.gsub(%r{/railties/.*}, '')).realpath
-  run "cp -r '#{rails_path}' vendor/rails"
-  run "rm -rf vendor/rails/.git"
-end
-
-log "initialized", "application structure"
 
 if @lego_options or yes?("Do you want to play LEGO?")
   all_yes = @lego_options ? false : yes?("Install everything without question?")
---
+++ basic.rb
@@ -0,0 +1,49 @@
+# remove tmp dirs
+run "rmdir tmp/{pids,sessions,sockets,cache}"
+
+# remove unnecessary stuff
+run "rm README log/*.log public/index.html public/images/rails.png"
+
+# keep empty dirs
+run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;")
+
+# init git repo
+git :init
+
+# basic .gitignore file
+file '.gitignore', 
+%q{log/*.log
+log/*.pid
+db/*.db
+db/*.sqlite3
+db/schema.rb
+tmp/**/*
+.DS_Store
+doc/api
+doc/app
+config/database.yml
+autotest_result.html
+coverage
+public/javascripts/*_[0-9]*.js
+public/stylesheets/*_[0-9]*.css
+public/attachments
+}
+
+# copy sample database config
+run "cp config/database.yml config/database.yml.sample"
+
+# commit changes
+git :add => "."
+git :commit => "-a -m 'Setting up a new rails app. Copy config/database.yml.sample to config/database.yml and customize.'"
+
+# freeze edge rails
+if respond_to?(:braid)
+  braid "git://github.com/rails/rails.git", "vendor/rails"
+else
+  # Guess full Rails path
+  rails_path = Pathname.new($LOAD_PATH.find {|p| p =~ /railties/}.gsub(%r{/railties/.*}, '')).realpath
+  run "cp -r '#{rails_path}' vendor/rails"
+  run "rm -rf vendor/rails/.git"
+end
+
+log "initialized", "application structure"
